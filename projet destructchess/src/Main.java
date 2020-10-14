a4c2bbc0eb4465e57eb5f051de3b9b06e52f7619
Kévin <dobrotakevin@yahoo.fr>
2020-10-14 11:18:27 +0200

    Choix numéro 3 fini

--- projet destructchess/src/Main.java
+++ projet destructchess/src/Main.java
@@ -3,20 +3,55 @@ import java.util.Scanner;
 public class Main
 {
 
-    public static void main(String[] args)
-    {
-        System.out.println("hello, les zamis");
-
+    public static void main(String[] args) {
+        //initialisation des variables local
         Scanner operateurA = new Scanner(System.in);
-        int operateur1;
+        int operateur1; //choix du joueur 1
+        int optionJoue = 1;
+        int optionRegles = 2;
+
+
+
+
+
+
+
 
         //Verification de l'entrée de l'utilisateur, afin que ce soit bien des chiffres de rentrés
+        System.out.println("Bonjour et bienvenu dans insert un nom, choisit un numéro pour navigué");
+
+        System.out.println(1 +" Joué");
+
+
+
+
+
+        System.out.println( 2 +" Régles");
+
+
+
+        // Choix pour stopper le programme
+        System.out.println(3 +" Quitté");{
+            if(operateurA.hasNextInt(3)){
+                System.exit(3);
+            }
+        }
+
+
+
         while (!operateurA.hasNextInt())
         {
             System.out.println("J'ai demandé des chiffres..");
             operateurA.next();
         }
+
+
+
+
         operateur1 = operateurA.nextInt();
-        System.out.println(operateur1);
+
+
+
+
     }
 }
