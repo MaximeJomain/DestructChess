9d0fcb921af830a90783eaa17701f0e8b2e2d63b
Naplyrroth <lecorbeiller.nathan@outlook.fr>
2020-10-14 10:03:30 +0200

    Commande pour s'assurer de ce qui est input par le joueur.

--- projet destructchess/src/Main.java
+++ projet destructchess/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
         System.out.println("hello, les zamis");
+
+        Scanner operateurA = new Scanner(System.in);
+        int operateur1;
+
+        //Verification de l'entrée de l'utilisateur, afin que ce soit bien des chiffres de rentrés
+        while (!operateurA.hasNextInt())
+        {
+            System.out.println("J'ai demandé des chiffres..");
+            operateurA.next();
+        }
+        operateur1 = operateurA.nextInt();
+        System.out.println(operateur1);
     }
 }
