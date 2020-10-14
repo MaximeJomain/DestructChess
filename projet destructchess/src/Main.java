e69d9a62dec4bfc9faa156b98e399c24b5dc9ced
MaximeJomain <maxime.jomain95@gmail.com>
2020-10-14 11:12:30 +0200

    Voici l'affichage du plateau de jeu.

--- projet destructchess/src/Main.java
+++ projet destructchess/src/Main.java
@@ -1,8 +1,41 @@
-public class Main
-{
+import java.sql.SQLOutput;
 
-    public static void main(String[] args)
-    {
-        System.out.println("hello, les zamis");
+//création de la story #03
+public class Main {
+
+    public static void main(String[] args) {
+        //initialisation du plateau de jeu
+        int colonne = 11 ;
+        int ligne = 10 ;
+        int i ;
+        int j ;
+        int [][] plateauDeJeu = new int [ligne][colonne] ;
+
+        //affichage du plateau de jeu
+        for ( i = 0; i < ligne; i++ ) {
+            for ( j = 0; j < colonne; j++ )  {
+                System.out.print("|" + plateauDeJeu[i][j] ) ;
+            }
+            System.out.println("|") ;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
