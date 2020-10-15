8ce9714a201845f933cae47fc3a5e701abc07c9c
johanna tarento <72390893+johannaT19@users.noreply.github.com>
2020-10-15 09:35:50 +0200

    Règles du jeu

--- projet destructchess/src/Règles.java
+++ projet destructchess/src/Règles.java
@@ -5,10 +5,10 @@ public class Règles
 
         //Règles de jeu
         System.out.println("Règles du jeu");
-        System.out.println("Les joueurs doivent éliminer leurs adversaire en réussissant a les bloquer par la destruction de cases a chaque tours.");
+        System.out.println("Les joueurs doivent éliminer leurs adversaires en réussissant à les bloquer par la destruction de cases à chaque tour.");
         System.out.println("1. Les joueurs peuvent se déplacer verticalement et horizontalement.");
-        System.out.println("2. les joueurs doivent détruire une case de la grille après leurs déplacement.");
-        System.out.println("3. les joueurs ne peuvent pas s'installer sur un case deja occuper.");
+        System.out.println("2. les joueurs doivent détruire une case de la grille après leur déplacement.");
+        System.out.println("3. les joueurs ne peuvent pas aller sur un case déjà occupée.");
 
     }
 }
